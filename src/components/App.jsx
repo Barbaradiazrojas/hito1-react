f1834fdf5a058700accd817262787e35481fe690
Barbara Diaz Rojas <barbaradiazrojas.bd@gmail.com>
2025-02-14 00:00:44 -0300

    correccion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Navbar from './Navbar'; // Corregido: ruta relativa
-import Home from './Home';    // Corregido: ruta relativa
-import Footer from './Footer'; // Corregido: ruta relativa
+import Navbar from './navbar'; // Corregido: ruta relativa
+import Home from './home';    // Corregido: ruta relativa
+import Footer from './footer'; // Corregido: ruta relativa
 
 const App = () => {
   return (
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './Header';
+import Header from '../assets/img/header.jpg';
 import CardPizza from './CardPizza';
 
 const Home = () => {
